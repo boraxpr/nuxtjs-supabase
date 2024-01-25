1678185d0bb59824921a209daa4f8b691b9f8a5e
boraxpr <tata-naipawat@hotmail.com>
2024-01-25 15:07:12 +0700

    Fix datatable

--- components/quotationList.vue
+++ components/quotationList.vue
@@ -7,17 +7,17 @@
       selectionMode="single"
     >
       <Column field="doc_num" header="Doc Num"></Column>
-      <!-- <Column field="created_date" header="Created Date"></Column> -->
-      <!-- <Column field="customers.name" header="Customer Name"></Column> -->
-      <!-- <Column field="status" header="Status"></Column> -->
-      <!-- <Column field="project_name" header="Project"></Column> -->
-      <!-- <Column field="grand_total" header="Grand Total"> -->
-      <!-- <template #body="slotProps">
+      <Column field="created_date" header="Created Date"></Column>
+      <Column field="customers.name" header="Customer Name"></Column>
+      <Column field="status" header="Status"></Column>
+      <Column field="project_name" header="Project"></Column>
+      <Column field="grand_total" header="Grand Total">
+        <template #body="slotProps">
           {{
             currencyFormat(slotProps.data.grand_total, slotProps.data.currency)
           }}
-        </template> -->
-      <!-- </Column> -->
+        </template>
+      </Column>
     </DataTable>
   </div>
 </template>
@@ -25,8 +25,6 @@
 <script setup>
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
-import ColumnGroup from "primevue/columngroup"; // optional
-import Row from "primevue/row"; // optional
 
 const { quotations } = defineProps(["quotations"]);
 </script>
