237d4f296a348422d5c32120af7ed4bee13e1eab
boraxpr <tata-naipawat@hotmail.com>
2024-01-29 15:10:28 +0700

    Init scaffold for line oauth

---
+++ components/LineLoginButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Button
+      :on-click="
+        () =>
+          navigateTo('', {
+            external: true,
+          })
+      "
+      >Line</Button
+    >
+  </div>
+</template>
+
+<script setup></script>
