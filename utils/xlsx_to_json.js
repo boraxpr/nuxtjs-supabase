0d523abb658a52d9150f9a6779ddb813c14ed266
boraxpr <tata-naipawat@hotmail.com>
2024-01-31 13:02:59 +0700

    xlsx_to_json tested

--- utils/xlsx_to_json.js
+++ utils/xlsx_to_json.js
@@ -1,7 +1,14 @@
-import * as XLSX from "xlsx";
+import { read, utils } from "xlsx";
+
 export default async function (xlsxFile) {
-  const workbook = XLSX.read(xlsxFile);
-  const sheetName = workbook.SheetNames;
-  const sheet = workbook.Sheets[sheetName];
-  return XLSX.utils.sheet_add_json(sheet);
+  console.log(xlsxFile);
+  const buffer = await xlsxFile.arrayBuffer();
+
+  const workbook = read(buffer, { type: "buffer" });
+  console.log(workbook);
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  console.log(JSON.stringify(workbook.SheetNames));
+  const jsonArray = utils.sheet_to_json(worksheet);
+  console.log(jsonArray);
+  return jsonArray;
 }
