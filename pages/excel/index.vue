8ec014872be1c839f3b50c55e7a7c2ed26db35d8
boraxpr <tata-naipawat@hotmail.com>
2024-01-31 13:02:59 +0700

    xlsx_to_json tested

--- pages/excel/index.vue
+++ pages/excel/index.vue
@@ -1,33 +1,51 @@
 <template>
-  <div class="card flex justify-content-center">
+  <div class="card flex-col justify-content-center">
     <Toast />
-    <FileUpload
-      mode="basic"
-      @select="onSelect"
-      accept=".xlsx"
-      :maxFileSize="1000000"
-    />
-    <div>{{ records }}</div>
+    <div>
+      <FileUpload
+        mode="basic"
+        @select="onSelect"
+        accept=".xlsx"
+        :maxFileSize="1000000"
+      />
+    </div>
+    <div class="m-5">
+      <div v-for="(record, index) in records" :key="index">
+        {{ record }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import csv_to_json from "~/utils/csv_to_json";
+import xlsx_to_json from "~/utils/xlsx_to_json";
 
 const toast = useToast();
-const file = ref();
+const file = ref(File);
 const records = ref();
 const onSelect = async (event) => {
   file.value = event.files[0];
   console.log("HEY " + file.value);
-  const { recordData } = await csv_to_json(file.value);
-  toast.add({
-    severity: "info",
-    summary: "Success",
-    detail: `Excel Imported Successfully`,
-    life: 3000,
-  });
-  records.value = JSON.stringify(recordData);
-  console.log(records.value);
+  try {
+    const jsonArray = await xlsx_to_json(file.value);
+    toast.add({
+      severity: "info",
+      summary: "Success",
+      detail: `Excel Imported Successfully`,
+      life: 3000,
+    });
+
+    console.log(typeof jsonArray);
+    records.value = jsonArray;
+    console.log(jsonArray);
+  } catch (error) {
+    console.error("Error processing Excel file:", error);
+    toast.add({
+      severity: "error",
+      summary: "Error",
+      detail: `Failed to process Excel file: ${error.message}`,
+      life: 5000,
+    });
+  }
 };
 </script>
