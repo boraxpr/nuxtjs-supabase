7ec8e9b69a75110b142d98b45aa0e77c7b97530d
ChalermchaiSphere <156997790+ChalermchaiSphere@users.noreply.github.com>
2024-02-01 14:33:16 +0700

    add dropdown and ui product list page

--- pages/products/create.vue
+++ pages/products/create.vue
@@ -3,7 +3,7 @@
         <header>
             <div class="Container grid grid-cols-2 gap-4 mb-4 h-40">
               <div class="Container flex items-center">
-                <div class="text-5xl">Create New Product</div>
+                <div class="text-5xl">Product Detail</div>
               </div>
               <div class="Container flex justify-end items-end">
 
@@ -12,7 +12,7 @@
         </header>
         <main>
             <div class="p-6 border-2 shadow-md rounded-lg">
-              <div class="grid grid-cols-3 gap-4 mb-4">
+              <div class="grid grid-cols-2 gap-4 mb-4">
                 <div>
                   <div>
                     <label for="">Product Name</label>
@@ -37,8 +37,6 @@
                     <input v-model="product_code" type="number" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
                   </div>
                 </div>
-              </div>
-              <div class="grid grid-cols-3 gap-4 mb-4">
                 <div>
                   <div>
                     <label for="">Category</label>
@@ -63,8 +61,6 @@
                     <input v-model="barcode" type="text" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
                   </div>
                 </div>
-              </div>
-              <div class="grid grid-cols-3 gap-4 mb-4">
                 <div>
                   <div>
                     <label for="">Selling Price</label>
@@ -89,8 +85,6 @@
                     <input v-model="product_desc" type="text" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
                   </div>
                 </div>
-              </div>
-              <div class="grid grid-cols-2 gap-4 mb-4">
                 <div>
                   <div>
                     <label for="">Income Account</label>
@@ -108,6 +102,84 @@
                   </div>
                 </div>
               </div>
+              <!-- <div class="grid grid-cols-2 gap-4 mb-4">
+                <div>
+                  <div>
+                    <label for="">Product Code</label>
+                  </div>
+                  <div class="Container mt-2">
+                    <input v-model="product_code" type="number" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label for="">Category</label>
+                  </div>
+                  <div class="Container mt-2">
+                    <input v-model="category" type="text" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label for="">Main Unit</label>
+                  </div>
+                  <div class="Container mt-2">
+                    <input v-model="main_unit" type="number" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label for="">Barcode</label>
+                  </div>
+                  <div class="Container mt-2">
+                    <input v-model="barcode" type="text" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
+                  </div>
+                </div>
+              </div> -->
+              <!-- <div class="grid grid-cols-3 gap-4 mb-4">
+                <div>
+                  <div>
+                    <label for="">Selling Price</label>
+                  </div>
+                  <div class="Container mt-2">
+                    <input v-model="selling_price" type="number" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label for="">Vat</label>
+                  </div>
+                  <div class="Container mt-2">
+                    <input v-model="vat" type="number" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label for="">Product Description</label>
+                  </div>
+                  <div class="Container mt-2">
+                    <input v-model="product_desc" type="text" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
+                  </div>
+                </div>
+              </div> -->
+              <!-- <div class="grid grid-cols-2 gap-4 mb-4">
+                <div>
+                  <div>
+                    <label for="">Income Account</label>
+                  </div>
+                  <div class="Container mt-2">
+                    <input v-model="income_account" type="text" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label for="">Unit</label>
+                  </div>
+                  <div class="Container mt-2">
+                    <input v-model="unit" type="text" class="p-2.5 h-10 w-full bg-gray-100 rounded-lg hover:border-2">
+                  </div>
+                </div>
+              </div> -->
               <div>
                   <div>
                     <label for="">Product Img</label>
@@ -151,7 +223,7 @@
                   </div>
                 </div>
             </div>
-            <div class="flex justify-end mt-7 gap-3">
+            <div class="flex justify-center mt-7 gap-3 min-h-16">
               <div>
                 <Nuxt-link :to="`/products`">
                     <button class="border shadow-md border-solid rounded-[30px] text-red-600 border-red-600 h-10 w-24 hover:bg-gray-100">CANCEL</button>
