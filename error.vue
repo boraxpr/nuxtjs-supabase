9bec876d8e972c273048f34657022c703e98b872
boraxpr <tata-naipawat@hotmail.com>
2024-02-02 10:04:51 +0700

    fix error.vue / conforms to https://nuxt.com/docs/guide/directory-structure/error

--- error.vue
+++ error.vue
@@ -12,23 +12,23 @@ const props = defineProps({
   >
     <div class="lg:col-span-10">
       <div class="flex flex-col items-center justify-center">
-        <p class="text-base font-semibold text-primary">404</p>
+        <p class="text-base font-semibold text-primary">
+          {{ error.statusCode }}
+        </p>
         <h1
           class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl"
         >
-          Error
+          ERROR
         </h1>
         <p
           class="mt-6 text-base/7 text-gray-500 dark:text-gray-400 text-center"
         >
-          Page not found
+          {{ error.statusMessage }}
         </p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <NuxtLink to="/"
-            ><Button type="button" class="inline-flex items-center">
-              Go back home</Button
-            ></NuxtLink
-          >
+          <NuxtLink to="/">
+            <Button class="inline-flex items-center"> Go back home</Button>
+          </NuxtLink>
         </div>
       </div>
     </div>
