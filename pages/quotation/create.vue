73f1c709bd1bb42dfdc268adf6322bed637cf305
boraxpr <tata-naipawat@hotmail.com>
2024-02-02 17:01:49 +0700

    Add to quotation create

--- pages/quotation/create.vue
+++ pages/quotation/create.vue
@@ -6,7 +6,7 @@
       >
         <!-- Header -->
         <div>
-          <div className="text-left text-3xl">Create Quotation</div>
+          <div className="text-left text-3xl font-semibold">Add Quotation</div>
         </div>
         <div class="grid grid-cols-3 gap-2 print:hidden">
           <Button
@@ -86,14 +86,89 @@
         class="w-11/12 print:w-11/12 mx-auto p-4 m-2 shadow-md rounded-md border"
       >
         <div class="m-5 mt-0 flex flex-row justify-between space-x-5">
-          <div class="w-1/2">
-            <div class="mb-4 w-1/2 space-y-2">
-              <Dropdown v-model="selectedCustomer"> </Dropdown>
+          <div class="w-1/2 space-y-2">
+            <div class="flex flex-col w-1/2">
+              <div class="md:w-14rem space-y-2">
+                <label for="dd-customer">Customer Name</label>
+                <Dropdown
+                  v-model="selectedCity"
+                  inputId="dd-customer"
+                  :options="cities"
+                  optionLabel="name"
+                  class="w-full"
+                />
+              </div>
+            </div>
+
+            <label> Client Detail </label>
+            <div class="space-y-2">
+              <Textarea
+                rows="3"
+                autoResize
+                class="w-full"
+                placeholder="Address"
+              />
+
+              <div class="flex flex-row justify-between">
+                <InputText placeholder="Zip Code" class="w-[45%]" />
+                <InputText placeholder="Tax ID" class="w-[45%]" />
+              </div>
+              <div class="flex flex-row justify-between">
+                <InputText placeholder="Branch Name" class="w-[45%]" />
+                <InputText placeholder="Branch Code" class="w-[45%]" />
+              </div>
             </div>
           </div>
           <div class="w-[40%]">
             <div class="mb-4 space-y-2">
-              <div class="mb-4"></div>
+              <div class="mb-4 border">
+                <div class="text-right">Grand Total:</div>
+                <div class="text-right text-orange-400 font-bold text-xl">
+                  20000
+                </div>
+              </div>
+              <div class="flex flex-row justify-between">
+                <label>Date:</label>
+                <Calendar
+                  v-model="icondisplay"
+                  showIcon
+                  iconDisplay="input"
+                  class="w-[70%]"
+                />
+              </div>
+              <div class="flex flex-row justify-between">
+                <label>Credit (Day):</label>
+                <InputText class="w-[70%]"> </InputText>
+              </div>
+              <div class="flex flex-row justify-between">
+                <label>Due Date:</label>
+                <Calendar
+                  v-model="icondisplay"
+                  showIcon
+                  iconDisplay="input"
+                  class="w-[70%]"
+                />
+              </div>
+              <div class="flex flex-row justify-between">
+                <label>Sales Name:</label>
+                <InputText
+                  showIcon
+                  iconDisplay="input"
+                  class="w-[70%]"
+                  placeholder="Somchai Default"
+                  disabled
+                />
+              </div>
+              <div class="flex flex-row justify-between">
+                <label>Currency:</label>
+                <Dropdown
+                  v-model="selectedCurrency"
+                  inputId="dd-customer"
+                  :options="currencies"
+                  optionLabel="name"
+                  class="w-[70%]"
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -103,13 +178,83 @@
       >
         <div class="flex flex-row space-x-3">
           <div class="mb-4 w-9/12">
-            <div class="flex flex-row space-x-1 items-center"></div>
+            <div class="flex flex-row space-x-1 items-center">
+              <div class="grid grid-rows-2 w-full">
+                <label> Projects </label>
+                <Dropdown
+                  v-model="selectedProject"
+                  inputId="dd-customer"
+                  :options="projects"
+                  placeholder="Select a Project"
+                  optionLabel="name"
+                  class="w-full"
+                />
+              </div>
+            </div>
           </div>
           <div class="mb-4 w-3/12">
-            <div class="flex flex-row space-x-1 items-center"></div>
+            <div class="flex flex-row space-x-1 items-center">
+              <div class="grid grid-rows-2 w-full">
+                <label> Ref </label>
+                <InputText
+                  showIcon
+                  iconDisplay="input"
+                  placeholder=""
+                  disabled
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <div class="flex flex-row space-x-5 items-center"></div>
+        <div class="flex flex-row space-x-5 items-center">
+          <Textarea placeholder="Detail" rows="4" autoResize class="w-full">
+          </Textarea>
+        </div>
+      </div>
+      <div
+        class="w-11/12 print:w-11/12 mx-auto p-4 m-2 bg-card shadow-md rounded-md border"
+      >
+        <DataTable
+          :value="products"
+          editMode="cell"
+          @cell-edit-complete="onCellEditComplete"
+          :pt="{
+            table: { style: 'min-width: 50rem' },
+            column: {
+              bodycell: ({ state }) => ({
+                class: [{ 'pt-0 pb-0': state['d_editing'] }],
+              }),
+            },
+          }"
+        >
+          <Column
+            v-for="col of columns"
+            :key="col.field"
+            :field="col.field"
+            :header="col.header"
+            style="width: 25%"
+          >
+            <template #body="{ data, field }">
+              {{
+                field === "price" ? formatCurrency(data[field]) : data[field]
+              }}
+            </template>
+            <template #editor="{ data, field }">
+              <template v-if="field !== 'price'">
+                <InputText v-model="data[field]" autofocus />
+              </template>
+              <template v-else>
+                <InputNumber
+                  v-model="data[field]"
+                  mode="currency"
+                  currency="USD"
+                  locale="en-US"
+                  autofocus
+                />
+              </template>
+            </template>
+          </Column>
+        </DataTable>
       </div>
 
       <div
@@ -150,7 +295,25 @@
 useHead({
   title: "Quotation - Create",
 });
+const currentSales = await useSupabaseClient().auth.getUser().email;
 const selectedCustomer = ref();
+const selectedCurrency = ref();
+const selectedProject = ref();
+const products = ref();
+const columns = ref([
+  { field: "code", header: "Code" },
+  { field: "name", header: "Name" },
+  { field: "quantity", header: "Quantity" },
+  { field: "price", header: "Price" },
+]);
+const currencies = ref([
+  { name: "Thai Baht", code: "THB" },
+  { name: "US Dollar", code: "USD" },
+]);
+const projects = ref([
+  { name: "Sphere Soft 2025" },
+  { name: "Sphere Soft 2024" },
+]);
 </script>
 <style scoped>
 .line-file1-a {
