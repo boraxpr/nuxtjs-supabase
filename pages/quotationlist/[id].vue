17a4848111180d4648b66727e580887802f86747
pjn3w <patarapong.jom@gmail.com>
2024-02-07 14:12:01 +0700

    1

---
+++ pages/quotationlist/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+useHead({
+  title: `Quotation - ${useRoute().params.id}`,
+});
+</script>
+
+<style></style>
---
+++ pages/testpopup/index.vue
@@ -0,0 +1,44 @@
+
+<template>
+    <Toast />
+    <ConfirmPopup group="templating">
+        <template #message="slotProps">
+            <div class="flex flex-column align-items-center w-full gap-3 border-bottom-1 surface-border p-3 mb-3 pb-0">
+                <i :class="slotProps.message.icon" class="text-6xl text-primary-500"></i>
+                <p>{{ slotProps.message.message }}</p>
+            </div>
+        </template>
+    </ConfirmPopup>
+    <div class="card flex justify-content-center">
+        <Button @click="showTemplate($event)" label="Save"></Button>
+    </div>
+</template>
+
+<script setup>
+import { useConfirm } from "primevue/useconfirm";
+import { useToast } from "primevue/usetoast";
+
+const confirm = useConfirm();
+const toast = useToast();
+
+const showTemplate = (event) => {
+    confirm.require({
+        target: event.currentTarget,
+        group: 'templating',
+        message: 'Please confirm to proceed moving forward.',
+        icon: 'pi pi-exclamation-circle',
+        acceptIcon: 'pi pi-check',
+        rejectIcon: 'pi pi-times',
+        acceptLabel: 'Confirm',
+        rejectLabel: 'Cancel',
+        rejectClass: 'p-button-outlined p-button-sm',
+        acceptClass: 'p-button-sm',
+        accept: () => {
+            toast.add({severity:'info', summary:'Confirmed', detail:'You have accepted', life: 3000});
+        },
+        reject: () => {
+            toast.add({severity:'error', summary:'Rejected', detail:'You have rejected', life: 3000});
+        }
+    });
+}
+</script>
