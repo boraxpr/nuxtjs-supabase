f41283fbfe7b9cb9420bcd2b8f2b4f0b006bd712
boraxpr <tata-naipawat@hotmail.com>
2024-02-08 13:14:02 +0700

    Add an edge case to currencyFormat for a null currencyCode case to prevent error

--- utils/currencyFormat.ts
+++ utils/currencyFormat.ts
@@ -1,5 +1,8 @@
 // It will be available as randomEntry() (camelCase of file name without extension)
 export default function (number: number, currencyCode: string) {
+  if (currencyCode === "" || currencyCode === null) {
+    return "";
+  }
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: currencyCode,
