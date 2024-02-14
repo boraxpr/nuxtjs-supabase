1b36b135ee2ad1f770daaa07d960167cf6cb5cd9
ChalermchaiSphere <156997790+ChalermchaiSphere@users.noreply.github.com>
2024-02-14 09:59:43 +0700

    add edit message in confirm widget

--- components/confirm.vue
+++ components/confirm.vue
@@ -1,15 +1,15 @@
 <template>
-  <Dialog  v-model:visible="visible" modal :pt="{ root: 'border-none', mask: { style: 'backdrop-filter: blur(2px)'}}">
+  <Dialog  v-model:visible="confirm.visible" modal :pt="{ root: 'border-none', mask: { style: 'backdrop-filter: blur(2px)'}}">
       <template #container>
         <div class="flex flex-col px-5 py-7 gap-4">
           <div class="inline-flex flex-col gap-2">
             <div class="text-4xl font-semibold text-center p-10">
-              Confirm Save ?
+              {{ message }}
             </div>
           </div>
           <div class="flex items-center gap-3">
-              <Button label="Cancel" @click.prevent="callOnFunc(false)" text class="text-black border border-gray-300 shadow-md border-solid rounded-[24px] h-[54px] w-[215px] hover:bg-gray-200"></Button>
-              <Button label="Confirm" @click.prevent="callOnFunc(true)" text class="border bg-[#F17121] shadow-md border-solid rounded-[24px] text-white h-[54px] w-[215px] hover:bg-gray-200"></Button>
+              <Button :label="confirm.cancelBtn" @click.prevent="callOnFunc(false)" text class="text-black border border-gray-300 shadow-md border-solid rounded-[24px] h-[54px] w-[215px] hover:bg-gray-200"></Button>
+              <Button :label="confirm.confirmBtn" @click.prevent="callOnFunc(true)" text class="border bg-[#F17121] shadow-md border-solid rounded-[24px] text-white h-[54px] w-[215px] hover:bg-gray-200"></Button>
           </div>
       </div>
       </template>
@@ -19,22 +19,47 @@
 <script setup>
 
 const props = defineProps({
+  message: String,
+  confirmBtn: String,
+  cancelBtn: String,
   IsActive: Boolean
 })
-const visible = ref(false);
-
-watch( 
-  () => props.IsActive,
-  () => {
-    if(props.IsActive){
-      visible.value = true
-    }else{
-      visible.value = false
-    }
+const confirm = reactive({
+  visible: ref(false),
+  confirmBtn: ref(),
+  cancelBtn: ref()
 })
+// const visible = ref(false);
+const SetDefault = () => {
+  if(props.confirmBtn == undefined || props.confirmBtn == ""){
+    console.log("confirmBtn");
+    confirm.confirmBtn = "Confirm"
+  }else{
+    confirm.confirmBtn = props.confirmBtn
+  }
+  if(props.cancelBtn == undefined || props.cancelBtn == ""){
+    console.log("cancelBtn");
+    confirm.cancelBtn = "Cancel"
+  }else{
+    confirm.cancelBtn = props.cancelBtn
+  }
+}
+
 const emit = defineEmits(['confirmFunc'])
 
 const callOnFunc = (value) => {
   emit('confirmFunc',value)
 }
+
+SetDefault();
+
+watch( 
+  () => props.IsActive,
+  () => {
+    if(props.IsActive){
+      confirm.visible = true
+    }else{
+      confirm.visible = false
+    }
+})
 </script>
